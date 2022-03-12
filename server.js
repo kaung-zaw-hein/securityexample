0199fb521308a4210b0656c7d7ed2d6aeca00699
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-03-12 14:18:24 +0630

    check logged in,logout,save session

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const express = require('express');
@@ -14,9 +15,9 @@ const PORT = 3000;
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
-  COOKIE_KEY1: process.env.COOKIE_KEY1,
-  COOKIE_KEY2: process.env.COOKIE_KEY2
-}
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+};
 
 const AUTH_OPTIONS = {
   callbackURL: '/auth/google/callback',
@@ -31,20 +32,34 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
+// Save the session to the cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Read the session from the cookie
+passport.deserializeUser((id, done) => {
+  // User.findById(id).then(user => {
+  //   done(null, user);
+  // });
+  done(null, id);
+});
+
 const app = express();
 
 app.use(helmet());
 
 app.use(cookieSession({
-  nane:'session',
+  name: 'session',
   maxAge: 24 * 60 * 60 * 1000,
-  keys: [ config.COOKIE_KEY1, config.COOKIE_KEY2 ],
-}))
-
+  keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2 ],
+}));
 app.use(passport.initialize());
+app.use(passport.session());
 
-function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true; //TODO
+function checkLoggedIn(req, res, next) { 
+  console.log('Current user is:', req.user);
+  const isLoggedIn = req.isAuthenticated() && req.user;
   if (!isLoggedIn) {
     return res.status(401).json({
       error: 'You must log in!',
@@ -58,18 +73,21 @@ app.get('/auth/google',
     scope: ['email'],
   }));
 
-  app.get('/auth/google/callback', 
+app.get('/auth/google/callback', 
   passport.authenticate('google', {
     failureRedirect: '/failure',
     successRedirect: '/',
-    session: false,
+    session: true,
   }), 
   (req, res) => {
     console.log('Google called us back!');
   }
 );
 
-app.get('/auth/logout', (req, res) => {});
+app.get('/auth/logout', (req, res) => {
+  req.logout(); //Removes req.user and clears any logged in session
+  return res.redirect('/');
+});
 
 app.get('/secret', checkLoggedIn, (req, res) => {
   return res.send('Your personal secret value is 42!');
@@ -83,6 +101,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
